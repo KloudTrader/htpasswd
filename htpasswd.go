ed9d41c87186679bef2a9d4d8a6ce0afb3cf1dce
Immortalin <Immortalin@icloud.com>
2018-06-07 00:49:17 -0700

    update nginx sha512 prefix lacktherof

--- htpasswd.go
+++ htpasswd.go
@@ -61,7 +61,6 @@ func (hp HashedPasswords) SetPassword(name, password string, hashAlgorithm HashA
 	case HashBCrypt:
 		hash, err = hashBcrypt(password)
 	case HashSHA:
-		prefix = "{SHA}"
 		hash = hashSha(password)
 	}
 	if err != nil {
